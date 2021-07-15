eae14f3a7441dcb5cbbd0ba1b4bb1fe4ea61d0f1
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-07-15 16:25:47 +0700

    distance

--- src/components/services/map_card/map.js
+++ src/components/services/map_card/map.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import './map.css'
-import { withGoogleMap, withScriptjs, GoogleMap } from "react-google-maps"
+// import React from 'react'
+// import './map.css'
+// import { withGoogleMap, withScriptjs, GoogleMap } from "react-google-maps"
 
-const Map = () => {
-    return (
-        <div className="container_2">
-            <div className="map_wrapper">
-                {/* <div className="get_location">  
-                </div> */}
-                <div className="map">
-                    <GoogleMap
-                        defaultZoom={8}
-                        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-                        >
-                    </GoogleMap>
-                </div>
-            </div>
-        </div>
-    )
-}
+// const Map = () => {
+//     return (
+//         <div className="container_2">
+//             <div className="map_wrapper">
+//                 {/* <div className="get_location">
+//                 </div> */}
+//                 <div className="map">
+//                     <GoogleMap
+//                         defaultZoom={8}
+//                         defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//                         >
+//                     </GoogleMap>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default withScriptjs(withGoogleMap(Map));
+// export default withScriptjs(withGoogleMap(Map));
